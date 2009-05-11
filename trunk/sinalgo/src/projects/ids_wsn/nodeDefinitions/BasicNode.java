236d02a1332fae340b416b8362d4dbafd72fa1fa
marvinlemos <marvinlemos@84f4fcea-3dcc-11de-9b50-8bb2048eb44c>
2009-05-11 03:15:59 +0000

    Commit Inicial

---
+++ trunk/sinalgo/src/projects/ids_wsn/nodeDefinitions/BasicNode.java
@@ -0,0 +1,45 @@
+package projects.ids_wsn.nodeDefinitions;
+
+import sinalgo.configuration.WrongConfigurationException;
+import sinalgo.nodes.Node;
+import sinalgo.nodes.messages.Inbox;
+
+public class BasicNode extends Node {
+
+	@Override
+	public void checkRequirements() throws WrongConfigurationException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleMessages(Inbox inbox) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void neighborhoodChange() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postStep() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void preStep() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
